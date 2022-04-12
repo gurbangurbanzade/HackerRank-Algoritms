4911d6941e923f3310985878b89a5d6b52f023ee
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-04-13 00:12:48 +0400

    Jumping On The Clouds

---
+++ Interview Preparation Kit/Warm-up Challanges/JumpingOnTheClouds.js
@@ -0,0 +1,8 @@
+//c=[0 ,0 ,1, 0, 0, 1, 0] bu bir bulud oyunudur
+// 0 olanlar təhlükəsiz, 1 olanlar ildırımlı buludlardır
+// oyunçu başdan sona ən qısa neçə addıma çata bilər tapın
+// 1 ilə rastlaşdıqda növbəti buluda keçid edilir
+// 2 ardıcıl buludun üzərindən 1 dəfəyə adlamaq olar. Məsən
+// 0,0,0,1,0 burada sonuncu buluda 3 və 2 addıma gəlmək olar. cavab 2-dir. Çünki 1 ci buluddan 3 cü buluda bir başa keçid etmək olar
+// 0,1,0,1,0 burada sonuncu buluda 2 addıma gəlmək olar çünki ardıcıl təhlükəsiz bulud yoxdur
+//c=[0 ,0 ,1, 0, 0, 1, 0] bu Arreydə başdan sona neçə addıma gəlmək olar tapın//
