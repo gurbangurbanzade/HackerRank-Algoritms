123da387c9d524d92ddcc295a4c1fee4f197a51a
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-04-12 22:49:02 +0400

    Cütlənən ədədlər

---
+++ Interview Preparation Kit/Warm-up Challanges/SalesByMatch.js
@@ -0,0 +1,4 @@
+// ar = [10, 20, 20, 10, 10, 30, 50, 10, 20] Arreyində olan ədədlərdən
+// neçəsi bir-biri ilə cütlük təşkil edir? Nəzərə alın ki, ədədlər təkrarlanmamalıdır.
+// Məsələn. 10=10 (1), 10=10 (2), 20=20(3). Cavab 3. 3 dənə 20 ədədi olduğundan
+// 2-si bir-biri ilə cütlənir 3-cü tək qalır və hesaba alınmır.
