a2fc8b28be5e671d0d7477c3e69f7f09b6eb5a79
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-04-12 23:35:45 +0400

    Counting Valleys

---
+++ Interview Preparation Kit/Warm-up Challanges/CountingValleys.js
@@ -0,0 +1,8 @@
+//Burada U hərfləri 1 pillə yuxarı qalxır. D hərfləri 1 pillə aşağı düşür.
+//Təsəvvür edin ki, siz verilmiş arraydaki hərfləri icra edərək hərəkət edirsiniz.
+//Hərəkət yer səthi ilə başlayıb yer səthi ilə bitir. Yəni 0 ilə.
+//Aşağıda verilmiş addım sayına uğun neçə çökəklik keçdiyinizi tapın.
+//Yer çökəklik sayı yer səthindən 1 dəfə aşağı enib sonra yenidən səthə qayıtdıqda bitir. və 1 sayılır
+// atacağınız addımlar: UDDDUDUU . U=+1, D=-1. Yəni aşağıdakı kimi olacaq
+// 0-dan başlayırsınız +1-1-1-1+1-1+1+1. Cavab çökəklik sayı 1.
+// Çünki addım sayı 0-dan bir dəfə aşağı enir və ən sonda 0-a çata bilir.
