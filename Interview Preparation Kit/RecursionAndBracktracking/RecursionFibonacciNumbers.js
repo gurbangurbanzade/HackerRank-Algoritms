9e8ed93f267d66f5b6c865f41d0c80d8ff1a1fb3
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-04-13 13:14:56 +0400

    Recursion Fibonacci Numbers

---
+++ Interview Preparation Kit/RecursionAndBracktracking/RecursionFibonacciNumbers.js
@@ -0,0 +1,4 @@
+//Fibonacci ədəd sırasında daxil edilmiş sırada olan ədədi tapın
+// Bu sırada hər ədəd özündən əvvəlki 2 ədədin cəminə bərabərdir
+// f(0)=0, f(1)=1 bu həmişə belədir. Digərləri isə altdakı ardıcıllıq ilə sonsuz düzülür
+// Məsəslən 0,1,1,2,3,5,8,13,21 və s.
