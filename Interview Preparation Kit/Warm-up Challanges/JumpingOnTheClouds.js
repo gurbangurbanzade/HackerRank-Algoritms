efab76b69a641194b01670d50e8ffd12fc3c8be5
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-04-15 01:09:20 +0400

    7 Case işləyir

--- Interview Preparation Kit/Warm-up Challanges/JumpingOnTheClouds.js
+++ Interview Preparation Kit/Warm-up Challanges/JumpingOnTheClouds.js
@@ -6,3 +6,90 @@
 // 0,0,0,1,0 burada sonuncu buluda 3 və 2 addıma gəlmək olar. cavab 2-dir. Çünki 1 ci buluddan 3 cü buluda bir başa keçid etmək olar
 // 0,1,0,1,0 burada sonuncu buluda 2 addıma gəlmək olar çünki ardıcıl təhlükəsiz bulud yoxdur
 //c=[0 ,0 ,1, 0, 0, 1, 0] bu Arreydə başdan sona neçə addıma gəlmək olar tapın//
+let b = [
+  0,
+  0,
+  1,
+  0,
+  0,
+  0,
+  0,
+  1,
+  0,
+  0,
+  0,
+  0,
+  1,
+  0,
+  0,
+  0,
+  0,
+  0,
+  1,
+  0,
+  1,
+  0,
+  0,
+  0,
+  1,
+  0,
+  0,
+  1,
+  0,
+  ,
+  0,
+  0,
+  1,
+  0,
+  1,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  1,
+  0,
+  0,
+  1,
+  0,
+  1,
+  0,
+  0,
+];
+function jumpingOnClouds(c) {
+  let say = 0;
+  for (let i = 0; i < c.length - 1; i++) {
+    if (c[i] == 0) {
+      if (c[i + 1] == 0 || c[i + 1] == 1) {
+        say++;
+        if (c[i + 1] == 0 && c[i + 2] == 0 && c[i - 1] !== 0) {
+          say--;
+          if (c[i + 1] == 0 && c[i + 2] == 0 && c[i + 3] == 0) {
+            say--;
+          }
+        }
+      }
+    }
+  }
+  console.log(say);
+}
+jumpingOnClouds(b);
+
+// Hackerrank saytinda kod hissesine altdaki metni yazmaq lazimdir
+//
+//
+//  let say = 0;
+//   for (let i = 0; i < c.length - 1; i++) {
+//     if (c[i] == 0) {
+//       if (c[i + 1] == 0 || c[i + 1] == 1) {
+//         say++;
+//         if (c[i + 1] == 0 && c[i + 2] == 0) {
+//           say--;
+//         }
+//       }
+//     }
+//   }
+//   return say;
--- Interview Preparation Kit/Warm-up Challanges/SalesByMatch.js
+++ Interview Preparation Kit/Warm-up Challanges/SalesByMatch.js
@@ -2,3 +2,44 @@
 // neçəsi bir-biri ilə cütlük təşkil edir? Nəzərə alın ki, ədədlər təkrarlanmamalıdır.
 // Məsələn. 10=10 (1), 10=10 (2), 20=20(3). Cavab 3. 3 dənə 20 ədədi olduğundan
 // 2-si bir-biri ilə cütlənir 3-cü tək qalır və hesaba alınmır.
+let array = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+
+// function sockMerchant(n, ar) {
+//   let obj =
+//   console.log(obj);
+//   for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < n; j++) {
+//       if (i !== j) {
+//         if (ar[i] == ar[j]) {
+//           obj[ar[i]]++;
+//           Object.values(obj[ar[i]]);
+
+//           console.log(obj);
+//           break;
+//         }
+//       } else {
+//         break;
+//       }
+//     }
+//   }
+// }
+
+// sockMerchant(9, array);
+
+function sockMerchant(n, ar) {
+  ar = ar.sort((a, b) => a - b);
+  let say = 0;
+  let net;
+  console.log(ar);
+  for (let i = 0; i < ar.length - 1; i++) {
+    for (let j = i + 1; j < ar.length; j++) {
+      if (ar[i] == ar[j]) {
+        say++;
+        console.log(say);
+        break;
+      }
+    }
+  }
+}
+
+sockMerchant(9, array);
