1e0bf346afa1c3bba2f5651eb775ec2443c0264a
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-04-14 21:43:57 +0400

    Recursion: Fibonacci Numbers Task Done

--- Interview Preparation Kit/RecursionAndBracktracking/RecursionFibonacciNumbers.js
+++ Interview Preparation Kit/RecursionAndBracktracking/RecursionFibonacciNumbers.js
@@ -2,3 +2,19 @@
 // Bu sırada hər ədəd özündən əvvəlki 2 ədədin cəminə bərabərdir
 // f(0)=0, f(1)=1 bu həmişə belədir. Digərləri isə altdakı ardıcıllıq ilə sonsuz düzülür
 // Məsəslən 0,1,1,2,3,5,8,13,21 və s.
+
+function fibonacci(n) {
+  let arr = [0, 1];
+  for (let i = 1; i <= n * n * n * n * n; i++) {
+    for (let j = 1; j <= arr.length + 1; j++)
+      if (i == arr[arr.length - 1] + arr[arr.length - 2]) {
+        arr.push(i);
+        break;
+      } else {
+        break;
+      }
+  }
+  return console.log(arr[n]);
+}
+
+fibonacci(3);
